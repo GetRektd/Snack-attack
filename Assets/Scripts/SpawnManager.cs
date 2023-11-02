5593816a4a009e562ce6c83547949243ef9d1807
GetRektd <lgibb24@aoiths.org>
2023-11-02 14:09:32 -0400

    Snacky

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    
 
     public GameObject [] spherePrefabs;
 
-    
+    private float spawnInterval = 1.5f;
+
+    private float startDelay = 2;
+
+    private float spawnRangeX = 9;
+
+    private float spawnPosZ = 8;
+
+    private float spawnPosY  = 13;
 
 
 
@@ -15,6 +24,7 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        InvokeRepeating ("SpawnRandomBalls", startDelay, spawnInterval);
 
     }
 
@@ -23,5 +33,16 @@ public class SpawnManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void SpawnRandomBalls ()
+
+    {
+        int sphereIndex = Random.Range(0, spherePrefabs.Length);
+
+        Vector3 spawnpos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, Random.Range(-spawnPosZ, spawnPosZ) );
+
+        Instantiate(spherePrefabs[sphereIndex], spawnPosZ, spherePrefabs[sphereIndex].transform.rotation);
+
     }
 }
