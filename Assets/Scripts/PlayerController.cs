6cc119901d2d61c5b472397d7ccbc4898410667f
GetRektd <lgibb24@aoiths.org>
2023-11-02 13:06:19 -0400

    snacky

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private float speed = 20.0f;
+
+    private float turnSpeed = 45.0f;
+
+    private float horizontalInput;
+
+    private float forwardInput;
+
+    public bool hasCollectible;
+
+    float zRange = 8;
+
+    float xRange = 6;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+         horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        
+        transform.Translate(Vector3.right * turnSpeed * horizontalInput * Time.deltaTime);
+
+        
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if(transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+        if(transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        }
+        
+    }
+}
